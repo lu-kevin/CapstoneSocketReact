d50316f78e5deb8643598708e813926b527731e1
Kevin Lu <35882128+lu-kevin@users.noreply.github.com>
2023-03-29 12:04:36 -0400

    initial commit

---
+++ server/index.js
@@ -0,0 +1,34 @@
+const express = require("express");
+const app = express();
+const http = require("http");
+const { Server } = require("socket.io");
+const cors = require("cors");
+
+app.use(cors());
+
+const server = http.createServer(app);
+
+const io = new Server(server, {
+    cors: {
+        origin: "http://localhost:3002",
+        methods: ["GET", "POST"],
+    },
+});
+
+io.on("connection", (socket) => {
+    console.log(`User connected with id: ${socket.id}`);
+
+    socket.on("sendMsg", (data) => {
+        console.log(data);
+        socket.broadcast.emit("getMsg", data);
+    });
+
+    //socket.on("ReactCommand", (data) => {
+        //console.log(data);
+    //});
+
+});
+
+server.listen(3001, () => {
+    console.log("Server is running.");
+});
